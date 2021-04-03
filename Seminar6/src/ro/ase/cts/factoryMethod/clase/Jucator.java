57d5c384c7eff405021a76150a106299266519f2
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2021-04-03 12:24:39 +0300

    factory_method

---
+++ Seminar6/src/ro/ase/cts/factoryMethod/clase/Jucator.java
@@ -0,0 +1,36 @@
+package ro.ase.cts.factoryMethod.clase;
+
+public abstract class Jucator {
+    private String nume ;
+    private int nrTricou ;
+
+    public Jucator(String nume, int nrTricou) {
+        this.nume = nume;
+        this.nrTricou = nrTricou;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public int getNrTricou() {
+        return nrTricou;
+    }
+
+    public void setNrTricou(int nrTricou) {
+        this.nrTricou = nrTricou;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Jucator{");
+        sb.append("nume='").append(nume).append('\'');
+        sb.append(", nrTricou=").append(nrTricou);
+        sb.append('}');
+        return sb.toString();
+    }
+}
