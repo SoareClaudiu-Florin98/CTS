4a10479cdbf8f4b2c28dbcfa44f5dcefd96af5b1
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2021-04-10 13:04:30 +0300

    decorator

---
+++ Seminar7/src/ro/ase/cts/Decorator/Clase/DecoratorAbstract.java
@@ -0,0 +1,28 @@
+package ro.ase.cts.Decorator.Clase;
+
+public abstract class DecoratorAbstract implements CardBancar{
+    private CardBancar card ;
+
+    public DecoratorAbstract(CardBancar card) {
+        this.card = card;
+    }
+    public abstract void platesteContactless() ;
+
+    @Override
+    public void platestePOS() {
+        card.platestePOS();
+    }
+
+    public CardBancar getCard() {
+        return card;
+    }
+
+    public void setCard(CardBancar card) {
+        this.card = card;
+    }
+
+    @Override
+    public void platesteOnline() {
+        card.platesteOnline();
+    }
+}
