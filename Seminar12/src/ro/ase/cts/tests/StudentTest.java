dd014cb8b1f87e011df589a635023f03ec466ace
Soare Claudiu-Florin <soareclaudiuflorin@gmail.com>
2021-05-22 13:10:19 +0300

    unit tests2

--- Seminar12/src/ro/ase/cts/tests/StudentTest.java
+++ Seminar12/src/ro/ase/cts/tests/StudentTest.java
@@ -6,6 +6,7 @@ import ro.ase.cts.clase.Student;
 import static org.junit.Assert.*;
 
 public class StudentTest {
+    Student student ;
 
     @Test
     public void calculeazaMedie() {
@@ -37,8 +38,65 @@ public class StudentTest {
         Student student = new Student() ;
         int nota = 10 ;
         student.adaugaNota(nota);
-
-
     assertEquals(nota, student.getNota(0));
     }
+    @Test
+    public void testGetNota(){
+        Student student = new Student() ;
+        student.adaugaNota(10);
+        student.adaugaNota(5);
+        student.adaugaNota(9);
+        assertEquals(9, student.getNota(2));
+    }
+    @Test
+    public void testAdaugaOSinguraNota(){
+        Student student = new Student() ;
+        int nota = 10 ;
+        student.adaugaNota(nota);
+        assertEquals(1, student.getNote().size());
+    }
+    @Test
+    public void testAdaugaONotaIncorecta(){
+        Student student = new Student() ;
+        try {
+            student.adaugaNota(-1);
+            fail("nu trebuia sa ajunga aici. Metoda nu arunca exceptii") ;
+        }catch(IllegalArgumentException ex){
+        }
+        catch(Exception e){
+            fail("nu arunca o excerptie de tipul illegal argument") ;
+
+        }
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testAdaugaONotaIncorectaJunit4(){
+        Student student = new Student() ;
+        student.adaugaNota(-1);
+
+        }
+
+    @Test
+    public void testStudentulAreRestante(){
+        Student student = new Student() ;
+        student.adaugaNota(3);
+        boolean rezultat  = student.areRestante() ;
+        assertTrue(rezultat);
+
+    }
+    @Test
+    public void testStudentulNuAreRestante(){
+        Student student = new Student() ;
+        student.adaugaNota(6);
+        boolean rezultat  = student.areRestante() ;
+        assertFalse(rezultat);
+    }
+    @Test
+    public void testCalculeazaMedieCorect(){
+        Student student = new Student() ;
+        student.adaugaNota(6);
+        student.adaugaNota(5);
+        float rezultat  = student.calculeazaMedie() ;
+        assertEquals(5.5, rezultat, 0.1);
+    }
+
 }
