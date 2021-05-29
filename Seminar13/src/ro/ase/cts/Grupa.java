f0f35c3766bfe5cbb054b19a51ca94bbd954e74b
Soare Claudiu <soareclaudiuflorin@gmail.com>
2021-05-29 12:33:12 +0300

    unit tests

---
+++ Seminar13/src/ro/ase/cts/Grupa.java
@@ -0,0 +1,54 @@
+package ro.ase.cts;
+
+import java.util.ArrayList;
+import java.util.List;
+//O grupa contine maxim 35 de studenti
+// Numarul grupei pentru facultatea noastr
+public class Grupa {
+    private List<IStudent> studenti;
+    private int nrGrupa;
+
+    public Grupa(int nrGrupa){
+        if(nrGrupa<1000 || nrGrupa>1100){
+            throw new IllegalArgumentException() ;
+        }
+        else{
+            this.nrGrupa=nrGrupa;
+            studenti=new ArrayList<>();
+        }
+
+    }
+
+    public List<IStudent> getStudenti() {
+        return studenti;
+    }
+
+    public void importaStudenti(List<IStudent> studenti){
+        this.studenti.addAll(studenti);
+    }
+
+    public void adaugaStudent(IStudent student){
+        studenti.add(student);
+    }
+
+    public IStudent getStudent(int index){
+        if(index>=0 && index< studenti.size()){
+            return studenti.get(index);
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public float getPromovabilitate(){
+        int nrRestantieri=0;
+        for(IStudent student:studenti){
+            if(student.areRestante()){
+                nrRestantieri++;
+            }
+        }
+        return studenti.size()/nrRestantieri;
+    }
+
+    public int getNrGrupa() {
+        return this.nrGrupa ;
+    }
+}
